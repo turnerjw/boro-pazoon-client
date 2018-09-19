1b56466f1a06cd3291eb47a6bd7b09064c0fc1ba
Justin Turner <justinturner@Justins-MacBook-Pro.local>
2018-09-18 23:59:05 -0400

    Set up socket connection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
-import React from "react"
+import React, { Component } from "react";
+import io from "../../node_modules/socket.io-client";
 
-export default () => <div>Hello world!</div>
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            endpoint: "https://boro-pazoon-server-kvcqxgohop.now.sh"
+        };
+    }
+
+    componentDidMount() {
+        const { endpoint } = this.state;
+        const socket = io(endpoint);
+    }
+
+    render() {
+        return <div>Hello world!</div>;
+    }
+}
+
+export default App;
