c36ed0a1c645bcd82d88e8a37582209d3b671118
Andrew Sciro <afsciro@spscommerce.com>
2018-09-20 14:28:17 -0400

    sps style semantics

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,134 +1,144 @@
 import React, { Component } from "react";
 
 class Canvas extends Component {
-    constructor() {
-        super();
-        this.state = {
-            drawing: false,
-            current: {
-                x: null,
-                y: null,
-                color: 'black'
-            },
-            canvas: null,
-            context: null
-        };
+  constructor() {
+    super();
+    this.state = {
+      drawing: false,
+      current: {
+        x: null,
+        y: null,
+        color: "black"
+      },
+      canvas: null,
+      context: null
+    };
+  }
+
+  componentDidMount() {
+    const canvas = this.refs.canvas;
+    const context = canvas.getContext("2d");
+    context.font = "40px Courier";
+    context.fillText("Test Text", 210, 75);
+
+    canvas.addEventListener("mousedown", this.onMouseDown, false);
+    canvas.addEventListener("mouseup", this.onMouseUp, false);
+    canvas.addEventListener("mouseout", this.onMouseUp, false);
+    canvas.addEventListener(
+      "mousemove",
+      this.throttle(this.onMouseMove, 10),
+      false
+    );
+
+    this.setState({
+      canvas: canvas,
+      context: context
+    });
+  }
+
+  drawLine = (x0, y0, x1, y1, color, emit) => {
+    const { canvas, context } = this.state;
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = color;
+    context.lineWidth = 2;
+    context.stroke();
+    context.closePath();
+
+    if (!emit) {
+      return;
     }
+    var w = canvas.width;
+    var h = canvas.height;
 
-    componentDidMount() {
-        const canvas = this.refs.canvas;
-        const context = canvas.getContext("2d");
-        context.font = "40px Courier";
-        context.fillText("Test Text", 210, 75);
+    // socket.emit('drawing', {
+    //   x0: x0 / w,
+    //   y0: y0 / h,
+    //   x1: x1 / w,
+    //   y1: y1 / h,
+    //   color: color
+    // });
+  };
 
-        canvas.addEventListener("mousedown", this.onMouseDown, false);
-        canvas.addEventListener("mouseup", this.onMouseUp, false);
-        canvas.addEventListener("mouseout", this.onMouseUp, false);
-        canvas.addEventListener(
-            "mousemove",
-            this.throttle(this.onMouseMove, 10),
-            false
-        );
-
-        this.setState({
-            canvas: canvas,
-            context: context
-        })
-    }
-
-    drawLine = (x0, y0, x1, y1, color, emit) => {
-        const {canvas, context} = this.state;
-        context.beginPath();
-        context.moveTo(x0, y0);
-        context.lineTo(x1, y1);
-        context.strokeStyle = color;
-        context.lineWidth = 2;
-        context.stroke();
-        context.closePath();
-    
-        if (!emit) { return; }
-        var w = canvas.width;
-        var h = canvas.height;
-    
-        // socket.emit('drawing', {
-        //   x0: x0 / w,
-        //   y0: y0 / h,
-        //   x1: x1 / w,
-        //   y1: y1 / h,
-        //   color: color
-        // });
+  onMouseDown = e => {
+    this.setState({
+      drawing: true,
+      current: {
+        x: e.clientX,
+        y: e.clientY
       }
+    });
+  };
 
-    onMouseDown = e => {
-        this.setState({
-            drawing: true,
-            current: {
-                x: e.clientX,
-                y: e.clientY
-            }
-        });
-    };
-
-    onMouseUp = e => {
-        const { drawing, current } = this.state;
-        if (!drawing) {
-            return;
-        }
-
-        this.setState({
-            drawing: false
-        });
-
-        this.drawLine(
-            current.x,
-            current.y,
-            e.clientX,
-            e.clientY,
-            current.color,
-            true
-        );
-    };
-
-    onMouseMove = e => {
-        const { drawing, current } = this.state;
-        if (!drawing) {
-            return;
-        }
-        this.drawLine(
-            current.x,
-            current.y,
-            e.clientX,
-            e.clientY,
-            current.color,
-            true
-        );
-        this.setState({
-            current: {
-                x: e.clientX,
-                y: e.clientY
-            }
-        });
-    };
-
-    // limit the number of events per second
-    throttle = (callback, delay) => {
-        var previousCall = new Date().getTime();
-        return function() {
-            var time = new Date().getTime();
-
-            if (time - previousCall >= delay) {
-                previousCall = time;
-                callback.apply(null, arguments);
-            }
-        };
-    };
-
-    render() {
-        return (
-            <div>
-                <canvas ref="canvas" width={640} height={425} />
-            </div>
-        );
+  onMouseUp = e => {
+    const { drawing, current } = this.state;
+    if (!drawing) {
+      return;
     }
+
+    this.setState({
+      drawing: false
+    });
+
+    this.drawLine(
+      current.x,
+      current.y,
+      e.clientX,
+      e.clientY,
+      current.color,
+      true
+    );
+  };
+
+  onMouseMove = e => {
+    const { drawing, current } = this.state;
+    if (!drawing) {
+      return;
+    }
+    this.drawLine(
+      current.x,
+      current.y,
+      e.clientX,
+      e.clientY,
+      current.color,
+      true
+    );
+    this.setState({
+      current: {
+        x: e.clientX,
+        y: e.clientY
+      }
+    });
+  };
+
+  // limit the number of events per second
+  throttle = (callback, delay) => {
+    var previousCall = new Date().getTime();
+    return function() {
+      var time = new Date().getTime();
+
+      if (time - previousCall >= delay) {
+        previousCall = time;
+        callback.apply(null, arguments);
+      }
+    };
+  };
+
+  render() {
+    return (
+      <div className="sps-card">
+        <div className="sps-card__header">
+          <h4 className="sps-card__title">
+            <i className="sps-icon sps-icon-hat" aria-hidden="true" />
+            Your own little canvas
+          </h4>
+        </div>
+        <div className="sps-card__body">
+          <canvas ref="canvas" width={640} height={425} />
+        </div>
+      </div>
+    );
+  }
 }
 export default Canvas;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,80 +3,96 @@ import io from "../../node_modules/socket.io-client";
 import YouTube from "react-youtube";
 import SPSHeader from "../components/header";
 import { ChromePicker } from "react-color";
+import "../styles/styles.css";
 import Canvas from "../components/canvas";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            endpoint: "https://boro-pazoon-server.glitch.me/",
-            videoData: {
-                timestamp: null,
-                id: null
-            },
-            color: {
-                hex: null
-            }
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "https://boro-pazoon-server.glitch.me/",
+      videoData: {
+        timestamp: null,
+        id: null
+      },
+      color: {
+        hex: null
+      }
+    };
+  }
 
-    componentDidMount() {
-        const { endpoint } = this.state;
-        const socket = io(endpoint);
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = io(endpoint);
+    const styles = socket.emit("add user", "Bob Ross");
 
-        socket.emit("add user", "Bob Ross");
+    socket.on("user joined", data => {
+      console.log(data);
+    });
 
-        socket.on("user joined", data => {
-            console.log(data);
-        });
+    socket.on("user left", data => {
+      console.log(data);
+    });
 
-        socket.on("user left", data => {
-            console.log(data);
-        });
+    socket.on("video data", data => {
+      console.log(data);
+      this.setState({
+        videoData: data
+      });
+    });
+  }
 
-        socket.on("video data", data => {
-            console.log(data);
-            this.setState({
-                videoData: data
-            });
-        });
-    }
+  handleColorChange = (color, event) => {
+    this.setState({
+      color: color
+    });
+  };
 
-    handleColorChange = (color, event) => {
-        this.setState({
-            color: color
-        });
+  render() {
+    const opts = {
+      height: "390",
+      width: "640",
+      playerVars: {
+        autoplay: 1,
+        controls: 0,
+        start: this.state.videoData.timestamp,
+        enablejsapi: true,
+        showinfo: 0
+      }
     };
 
-    render() {
-        const opts = {
-            height: "390",
-            width: "640",
-            playerVars: {
-                autoplay: 1,
-                controls: 0,
-                start: this.state.videoData.timestamp,
-                enablejsapi: true,
-                showinfo: 0
-            }
-        };
-
-        return (
-            <div className="sps-page sps-page--full-width">
-                <SPSHeader />
+    return (
+      <div className="sps-page sps-page--full-width">
+        <div className="sps-body sps-body--collapse-550">
+          <aside class="sps-body__sidebar sps-body__sidebar--400">
+            <SPSHeader />
+            <div className="sps-card">
+              <div class="sps-card__header">
+                <h4 class="sps-card__title">
+                  <i class="sps-icon sps-icon-heart" aria-hidden="true" />
+                  Bob Vision
+                </h4>
+              </div>
+              <div className="sps-card__body">
                 <YouTube
-                    videoId={this.state.videoData.id}
-                    opts={opts}
-                    onReady={this._onReady}
+                  videoId={this.state.videoData.id}
+                  opts={opts}
+                  onReady={this._onReady}
                 />
-                <ChromePicker
-                    color={this.state.color}
-                    onChange={this.handleColorChange}
-                />
-                <Canvas />
+              </div>
             </div>
-        );
-    }
+            <ChromePicker
+              color={this.state.color}
+              onChange={this.handleColorChange}
+            />
+          </aside>
+          <section class="sps-main-content sps-column-layout">
+            <Canvas />
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
