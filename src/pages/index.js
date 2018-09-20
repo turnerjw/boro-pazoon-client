b354653528f8e650159fcac564c30395e80a97d1
Justin William Turner <jturner@spscommerce.com>
2018-09-20 13:59:54 -0400

    Formatted index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,77 +6,77 @@ import { ChromePicker } from "react-color";
 import Canvas from "../components/canvas";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      endpoint: "https://boro-pazoon-server.glitch.me/",
-      videoData: {
-        timestamp: null,
-        id: null
-      },
-      color: {
-        hex: null
-      }
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            endpoint: "https://boro-pazoon-server.glitch.me/",
+            videoData: {
+                timestamp: null,
+                id: null
+            },
+            color: {
+                hex: null
+            }
+        };
+    }
 
-  componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = io(endpoint);
+    componentDidMount() {
+        const { endpoint } = this.state;
+        const socket = io(endpoint);
 
-    socket.emit("add user", "Bob Ross");
+        socket.emit("add user", "Bob Ross");
 
-    socket.on("user joined", data => {
-      console.log(data);
-    });
+        socket.on("user joined", data => {
+            console.log(data);
+        });
 
-    socket.on("user left", data => {
-      console.log(data);
-    });
+        socket.on("user left", data => {
+            console.log(data);
+        });
 
-    socket.on("video data", data => {
-      console.log(data);
-      this.setState({
-        videoData: data
-      });
-    });
-  }
+        socket.on("video data", data => {
+            console.log(data);
+            this.setState({
+                videoData: data
+            });
+        });
+    }
 
-  handleColorChange = (color, event) => {
-    this.setState({
-      color: color
-    });
-  };
-
-  render() {
-    const opts = {
-      height: "390",
-      width: "640",
-      playerVars: {
-        autoplay: 1,
-        controls: 0,
-        start: this.state.videoData.timestamp,
-        enablejsapi: true,
-        showinfo: 0
-      }
+    handleColorChange = (color, event) => {
+        this.setState({
+            color: color
+        });
     };
 
-    return (
-      <div className="sps-page sps-page--full-width">
-        <SPSHeader />
-        <YouTube
-          videoId={this.state.videoData.id}
-          opts={opts}
-          onReady={this._onReady}
-        />
-        <ChromePicker
-          color={this.state.color}
-          onChange={this.handleColorChange}
-        />
-        <Canvas />
-      </div>
-    );
-  }
+    render() {
+        const opts = {
+            height: "390",
+            width: "640",
+            playerVars: {
+                autoplay: 1,
+                controls: 0,
+                start: this.state.videoData.timestamp,
+                enablejsapi: true,
+                showinfo: 0
+            }
+        };
+
+        return (
+            <div className="sps-page sps-page--full-width">
+                <SPSHeader />
+                <YouTube
+                    videoId={this.state.videoData.id}
+                    opts={opts}
+                    onReady={this._onReady}
+                />
+                <ChromePicker
+                    color={this.state.color}
+                    onChange={this.handleColorChange}
+                />
+                <Canvas />
+            </div>
+        );
+    }
 }
 
 export default App;
