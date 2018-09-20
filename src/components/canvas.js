46d222e4aeb271bec6d39fb3fe86c0ed6fa783b7
Justin William Turner <jturner@spscommerce.com>
2018-09-20 13:52:04 -0400

    Implemented Drawing

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -7,22 +7,57 @@ class Canvas extends Component {
             drawing: false,
             current: {
                 x: null,
-                y: null
-            }
+                y: null,
+                color: 'black'
+            },
+            canvas: null,
+            context: null
         };
     }
 
     componentDidMount() {
         const canvas = this.refs.canvas;
-        const ctx = canvas.getContext("2d");
-        ctx.font = "40px Courier";
-        ctx.fillText("Test Text", 210, 75);
+        const context = canvas.getContext("2d");
+        context.font = "40px Courier";
+        context.fillText("Test Text", 210, 75);
 
         canvas.addEventListener("mousedown", this.onMouseDown, false);
         canvas.addEventListener("mouseup", this.onMouseUp, false);
-        canvas.addEventListener('mouseout', this.onMouseUp, false);
-        canvas.addEventListener('mousemove', throttle(this.onMouseMove, 10), false);
+        canvas.addEventListener("mouseout", this.onMouseUp, false);
+        canvas.addEventListener(
+            "mousemove",
+            this.throttle(this.onMouseMove, 10),
+            false
+        );
+
+        this.setState({
+            canvas: canvas,
+            context: context
+        })
     }
+
+    drawLine = (x0, y0, x1, y1, color, emit) => {
+        const {canvas, context} = this.state;
+        context.beginPath();
+        context.moveTo(x0, y0);
+        context.lineTo(x1, y1);
+        context.strokeStyle = color;
+        context.lineWidth = 2;
+        context.stroke();
+        context.closePath();
+    
+        if (!emit) { return; }
+        var w = canvas.width;
+        var h = canvas.height;
+    
+        // socket.emit('drawing', {
+        //   x0: x0 / w,
+        //   y0: y0 / h,
+        //   x1: x1 / w,
+        //   y1: y1 / h,
+        //   color: color
+        // });
+      }
 
     onMouseDown = e => {
         this.setState({
@@ -44,35 +79,48 @@ class Canvas extends Component {
             drawing: false
         });
 
-        // drawLine(
-        //     current.x,
-        //     current.y,
-        //     e.clientX,
-        //     e.clientY,
-        //     current.color,
-        //     true
-        // );
+        this.drawLine(
+            current.x,
+            current.y,
+            e.clientX,
+            e.clientY,
+            current.color,
+            true
+        );
     };
 
     onMouseMove = e => {
-        const { drawing } = this.state;
+        const { drawing, current } = this.state;
         if (!drawing) {
             return;
         }
-        // drawLine(
-        //     current.x,
-        //     current.y,
-        //     e.clientX,
-        //     e.clientY,
-        //     current.color,
-        //     true
-        // );
+        this.drawLine(
+            current.x,
+            current.y,
+            e.clientX,
+            e.clientY,
+            current.color,
+            true
+        );
         this.setState({
             current: {
                 x: e.clientX,
                 y: e.clientY
             }
         });
+    };
+
+    // limit the number of events per second
+    throttle = (callback, delay) => {
+        var previousCall = new Date().getTime();
+        return function() {
+            var time = new Date().getTime();
+
+            if (time - previousCall >= delay) {
+                previousCall = time;
+                callback.apply(null, arguments);
+            }
+        };
     };
 
     render() {
